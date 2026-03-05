9d3379831a0addc63d1b04ce84c2e984333dab5f
TengMengHouy <la0989975@gmail.com>
2026-03-05 13:31:51 +0700

    add file and folder to git

---
+++ center-server/configuration-server/src/test/java/com/etlife/configuration_server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etlife.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
