4eab0a20b1ea24cdba1085eede8acc2d80e5db27
TengMengHouy <la0989975@gmail.com>
2026-03-05 16:22:44 +0700

    add the form and pipeline service

---
+++ microservice/form-service/src/test/java/com/etlife/form_service/FormServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etlife.form_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
